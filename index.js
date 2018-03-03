6ed7f30dcb55af0bad6ad764ab37981874d6c7fa
neocydfr <franckboudraa@gmail.com>
2018-03-03 11:24:28 +0100

    testing ioredis

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
+
 const Redis = require('ioredis');
 const redis = new Redis(process.env.REDIS_URL);
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   redis.set('foo', 'bar');
   redis.get('foo', function(err, result) {
     res.send(result);
   });
+});*/
+
+app.use(express.json());
+
+app.post('/new', async (req, res) => {
+  try {
+    const data = await redis.get('foobar');
+    res.send(data);
+    console.log(data);
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
