abeaf4af915070aa07061d03108d3ba2db4f8e1a
neocydfr <franckboudraa@gmail.com>
2018-03-03 12:55:34 +0100

    elo prototype

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
-
 const Redis = require('ioredis');
-const redis = new Redis(process.env.REDIS_URL || 6379);
+const Elo = require('elo-js');
 
-const fetchCat = async id => {
+const app = express();
+app.use(express.json()); // handling post params
+const redis = new Redis(process.env.REDIS_URL || 6379); // connection to redis db
+
+const fetchCatScore = async id => {
   try {
     data = await redis.get(`cat_${id}`);
   } catch (error) {
@@ -20,15 +21,53 @@ const fetchCat = async id => {
 };
 
 app.post('/new', async (req, res) => {
-  score_cat_1 = await fetchCat(req.body.cat1);
-  score_cat_2 = await fetchCat(req.body.cat2);
+  /*
+  req.body.winner_id
+  req.body.looser_id
+  */
+  const elo = new Elo();
+  console.log('winner:', req.body.winner_id);
+  console.log('looser:', req.body.looser_id);
+
+  score_before_battle_for_winner = await fetchCatScore(req.body.winner_id);
+  score_before_battle_for_looser = await fetchCatScore(req.body.looser_id);
+
+  score_before_battle_for_winner = Number(score_before_battle_for_winner);
+  score_before_battle_for_looser = Number(score_before_battle_for_looser);
+
+  console.log(score_before_battle_for_winner);
+  console.log(score_before_battle_for_looser);
+
+  const score_after_battle_for_winner = elo.ifWins(
+    score_before_battle_for_winner,
+    score_before_battle_for_looser
+  );
+  const score_after_battle_for_looser = elo.ifLoses(
+    score_before_battle_for_looser,
+    score_after_battle_for_winner
+  );
+
+  console.log(score_after_battle_for_winner);
+  console.log(score_after_battle_for_looser);
 
   res.json({
     cats: [
-      { id: req.body.cat1, score: score_cat_1 },
-      { id: req.body.cat1, score: score_cat_1 }
+      {
+        id: req.body.winner_id,
+        scoreBefore: score_before_battle_for_winner,
+        scoreAfter: score_after_battle_for_winner
+      },
+      {
+        id: req.body.looser_id,
+        scoreBefore: score_before_battle_for_looser,
+        scoreAfter: score_after_battle_for_looser
+      }
     ]
   });
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send('Meeooowww!');
 });
 
 app.listen(process.env.PORT || 3000);
