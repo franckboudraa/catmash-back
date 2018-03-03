4cdf34c8fb4c403de1b522e886cb7eff0d9a7fe8
neocydfr <franckboudraa@gmail.com>
2018-03-03 12:16:31 +0100

    added fetchCat method with redis

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const express = require('express');
 const app = express();
-
-const Redis = require('ioredis');
-const redis = new Redis(process.env.REDIS_URL);
-
-/*app.get('/', (req, res) => {
-  redis.set('foo', 'bar');
-  redis.get('foo', function(err, result) {
-    res.send(result);
-  });
-});*/
-
 app.use(express.json());
 
-app.post('/new', async (req, res) => {
+const Redis = require('ioredis');
+const redis = new Redis(process.env.REDIS_URL || 6379);
+
+const fetchCat = async id => {
   try {
-    const data = await redis.get('foobar');
-    res.send(data);
-    console.log(data);
+    data = await redis.get(`cat_${id}`);
   } catch (error) {
-    res.send(error);
-    console.log(error);
+    res.send(503); // if Redis return an error
   }
+  if (!data) {
+    // if this cat doesn't exist yet in db, create one with a score of 1200
+    redis.set(`cat_${id}`, '1200');
+    return 1200;
+  }
+  return await data;
+};
+
+app.post('/new', async (req, res) => {
+  score_cat_1 = await fetchCat(req.body.cat1);
+  score_cat_2 = await fetchCat(req.body.cat2);
+
+  res.json({
+    cats: [
+      { id: req.body.cat1, score: score_cat_1 },
+      { id: req.body.cat1, score: score_cat_1 }
+    ]
+  });
 });
 
 app.listen(process.env.PORT || 3000);
