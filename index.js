d0149727f25599436631466ba9077a46b08551d2
neocydfr <franckboudraa@gmail.com>
2018-03-03 10:43:20 +0100

    added redis client

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+var client = require('redis').createClient(process.env.REDIS_URL);
 
 app.get('/', (req, res) => {
-  res.send('hi there');
+  client.set('string_key', 'string val', redis.print);
+  client.get('string_key', function(err, reply) {
+    // reply is null when the key is missing
+    console.log(reply);
+    res.json(reply);
+  });
 });
 
 app.listen(3000);
